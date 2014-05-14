2f1a742ce6bb5181ee2937b03f5e08031dfdd0f7
Emerson Roberts <eroberts@student.neumont.edu>
2014-05-14 09:21:35 -0600

    Added more to the pattern and methods for piece movement and placement. still need to fix the rest of the pattern

--- Chess/src/FileIO.java
+++ Chess/src/FileIO.java
@@ -5,12 +5,17 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class FileIO {
-	HashMap pieces = new HashMap();
+	HashMap pieces;
+	public static final Pattern CHESS_PATTERN = Pattern.compile("([A-Za-z][\\w*])(\\D\\w*)");
 	
 	public FileIO() {
+		pieces = new HashMap();
+		
 		pieces.put("K", "King");
 		pieces.put("Q", "Queen");
 		pieces.put("B", "Bishop");
@@ -22,10 +27,14 @@ public class FileIO {
 	}
 	
 	public static void main(String[] args) throws IOException {
+		FileIO fio = new FileIO();
+		fio.run(fio);
+	}
+
+	public void run(FileIO fio) throws IOException{
+		File file = new File("src/ChessInput.txt");
 		
-		File file = new File("src/chessArgs");
-		
-		FileInputStream fis = new FileInputStream("file");
+		FileInputStream fis = new FileInputStream(file);
 		InputStreamReader isr = new InputStreamReader(fis);
 		BufferedReader reader = new BufferedReader(isr);
 		
@@ -33,7 +42,40 @@ public class FileIO {
 		
 		while(newLine != null) {
 			
+			Matcher m = CHESS_PATTERN.matcher(newLine);
+			
+			while(m.find()) {
+				String piece = m.group(1);
+				String position = m.group(2);
+				
+				if(pieces.get(piece.substring(1)) != null) {
+					fio.piecePlacement(piece, position);
+				}else {
+					fio.pieceMovement(piece, position);
+				}
+			}
+			
 			newLine = reader.readLine();
 		}
+	}
+	
+	public void pieceMovement(String piece, String position) {
+		if(position.contains("*")) {
+			position = position.substring(1,3);
+			System.out.println("Moves the piece at " + piece.toUpperCase() + " to the square at " + position.toUpperCase() 
+					+ " and captures the piece at " + position.toUpperCase());
+		}else {
+			position = position.substring(1,3);
+			System.out.println("Moves the piece at " + piece.toUpperCase() + " to the square at " + position.toUpperCase());
+		}
+	}
+
+	public void piecePlacement(String piece, String position) {
+		String pieceName = (String) pieces.get(piece.substring(0,1));
+		
+		String pieceColor = (String) pieces.get(piece.substring(1));
+		
+		System.out.println("Place the " + pieceColor + " " + pieceName + " on " + position.toUpperCase());
+		
 	}
 }
