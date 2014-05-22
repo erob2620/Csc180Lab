c787c6dd09deb6dffa87ae5421dbbd121063a125
Emerson Roberts <eroberts@student.neumont.edu>
2014-05-22 09:07:47 -0600

    Changed the pattern as well as the if statement to handle an error that I was getting. also changed the multipiecemovement to make sure that it could accept bounds check.

--- Chess/src/ChessBoard.java
+++ Chess/src/ChessBoard.java
@@ -12,9 +12,10 @@ public class ChessBoard {
 
 	private final int BOARD_SIZE = 8;
 	private FileIO fio = new FileIO();
+	
 	private char[][] board = new char[BOARD_SIZE][BOARD_SIZE];
 	private File file;
-	public static final Pattern CHESS_PATTERN = Pattern.compile("(?<piece>[A-Za-z][ld0-9]\\s*)(?<position>\\w\\d\\W*\\s*)(?<pos2>\\w*\\d*\\s*)(?<pos3>\\w*\\d*)");
+	public static final Pattern CHESS_PATTERN = Pattern.compile("(?<piece>[A-Za-z][ld1-8]\\s*)(?<position>[a-h][1-8]\\W*\\s*)(?<pos2>\\w*\\d*\\s*)(?<pos3>\\w*\\d*)");
 
 	public static void main(String[] args) throws FileNotFoundException {
 		ChessBoard cb = new ChessBoard();
@@ -53,20 +54,43 @@ public class ChessBoard {
 			if (fio.pieces.get(piece.substring(1)) != null && positionTwo.equals("")) {
 				piecePlacement(piece, position);
 				
-			} else if (!positionTwo.equals("")) {
-				multiPieceMovement(piece, position, positionTwo,positionThree);
+			} else if (positionTwo.equals("") && positionThree.equals("")){
+				pieceMovement(piece, position);
+				
+			} else if(positionThree.equals("")) {
+				System.out.println("Invalid movement.");	
 				
 			} else {
-				pieceMovement(piece, position);
+				multiPieceMovement(piece, position, positionTwo,positionThree);
+				
 			}
 		}
 	}
 
 	private void multiPieceMovement(String position, String positionTwo, String positionThree, String positionFour) {
-		pieceMovement(position, positionTwo);
-		pieceMovement(positionThree, positionFour);
+		int col = charToNumber(position.charAt(0));
+		int row = stringToNum(position);
 		
+		int newRow = stringToNum(positionTwo);
+		int newCol = charToNumber(positionTwo.charAt(0));
 		
+		int pieceTwoCol = charToNumber(positionThree.charAt(0));
+		int pieceTwoRow = stringToNum(positionThree);
+		
+		int newRowPieceTwo = stringToNum(positionFour);
+		int newColPieceTwo = charToNumber(positionFour.charAt(0));
+		
+		if(boundsCheck(col, row) || boundsCheck(newRow, newCol)) {
+			if(boundsCheck(pieceTwoCol, pieceTwoRow) || boundsCheck(newRowPieceTwo, newColPieceTwo)) {
+				
+				pieceMovement(position, positionTwo);
+				pieceMovement(positionThree, positionFour);
+			}else {
+				System.out.println("The second piece movement was outside of the bounds of the board");
+			}
+		}else {
+			System.out.println("The first piece movement was outside the bounds of the board");
+		}
 	}
 	private void pieceMovement(String position, String positionTwo) {
 		int col = charToNumber(position.charAt(0));
@@ -75,8 +99,8 @@ public class ChessBoard {
 		int newRow = stringToNum(positionTwo);
 		int newCol = charToNumber(positionTwo.charAt(0));
 		
-		if(!boundsCheck(newCol, newRow) || !boundsCheck(col,row)) {
-			System.out.println("The move you tried to make was outside the bounds of the board.");
+		if(!boundsCheck(col,row) || !boundsCheck(newCol, newRow)) {
+			System.out.println("The piece you tried to move was out of the bounds of the board.");
 			
 		}else if(board[col][row] == '-'){
 			System.out.println("There was no piece to move at the location: " + position);
@@ -85,7 +109,7 @@ public class ChessBoard {
 			char piece = board[col][row];
 			board[col][row] = '-';
 			
-			placePiece(newRow, newCol, piece);
+			board[newCol][newRow] = piece;
 		}
 	}
 
@@ -95,13 +119,9 @@ public class ChessBoard {
 		int col = charToNumber(position.charAt(0));
 		int row = stringToNum(position);
 		
-		if(boundsCheck(col, row)) {
-			placePiece(row, col, pieceName);
-			fio.piecePlacement(piece, position);
+		
+		placePiece(row, col, pieceName);
 			
-		}else {
-			System.out.println("The move you tried to make was outside the bounds of the board.");
-		}
 	}
 
 	private void resetBoard() {
--- Chess/src/Piece.java
+++ Chess/src/Piece.java
@@ -4,9 +4,15 @@ public class Piece {
 	private int col;
 	private int row;
 	
-	public Piece(int col, int row) {
+	private String color;
+	private char name;
+	
+	public Piece(int col, int row, String color, char name) {
 		this.col = col;
 		this.row = row;
+		this.color = color;
+		
+		this.name = determineSide(color, name);
 	}
 
 	public int getCol() {
@@ -15,6 +21,9 @@ public class Piece {
 
 	public int getRow() {
 		return row;
+	}
+	private char determineSide(String color, char name) {
+		return (color.equals("l"))? Character.toUpperCase(name) : Character.toLowerCase(name);
 	}
 
 }
